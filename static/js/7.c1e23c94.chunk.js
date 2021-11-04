56d877b6afbd3392bf12344ae88f336396d92b89
Bellik888 <Bellik888@gmail.com>
2021-11-04 20:02:36 +0200

    Updates

---
+++ static/js/7.c1e23c94.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 7.c1e23c94.chunk.js.LICENSE.txt */
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[7],{67:function(t,o,s){"use strict"}}]);
+//# sourceMappingURL=7.c1e23c94.chunk.js.map
